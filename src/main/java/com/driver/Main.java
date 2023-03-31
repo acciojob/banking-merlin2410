becac72714703ef1fa0c32a7772293be7df0747d
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2023-03-31 16:16:29 +0530

    Submission 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,5 +3,18 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
+        try{
+            SavingsAccount s = new SavingsAccount("Merlin", 10000, 5000,5);
+            StudentAccount studentAccount = new StudentAccount("Arun",5000,"CET");
+            CurrentAccount currentAccount = new CurrentAccount("Rahul",15000,"AABACD");
+            s.withdraw(5000);
+            s.withdraw(5000);
+            currentAccount.validateLicenseId();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.toString());
+        }
+
     }
 }
