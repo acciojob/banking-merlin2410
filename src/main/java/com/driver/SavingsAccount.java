4a92cd45fb3b436c1f95222dd03b583dc355860b
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2023-04-01 10:59:07 +0530

    Submission 8

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -37,7 +37,7 @@ public class SavingsAccount extends BankAccount{
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
         if(amount>this.maxWithdrawalLimit)
-            throw new Exception("Maximum Withdraw Limit Exceed");
+            throw new Exception("Maximum Withdraw Limit Exceeded");
         if(amount>getBalance())
             throw new Exception("Insufficient Balance");
         setBalance(getBalance()-amount);
